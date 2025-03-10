66fe261b9744810d0f1c9308448b70aa6236dc4d
annaficotto <ficotto.2008@gmail.com>
2025-03-10 12:58:25 +0100

    Removed redirect.js, now managed by 404.html

--- redirect.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.pathname.endsWith("/")) {
-        let newPath = window.location.pathname + "index.html";
-        window.location.replace(newPath);
-    }
-});

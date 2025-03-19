8150b1a3ccf1bc1937a92bd1ddc6a3222ffb5213
annaficotto <ficotto.2008@gmail.com>
2025-03-19 20:13:02 +0100

    correzioni

--- auth.js
+++ auth.js
@@ -1,20 +1,28 @@
 const utentiAutorizzati = ["annafic8@gmail.com", "ficotto.2008@gmail.com"];
 
 function onGoogleLogin(response) {
-    const token = response.credential;
-    fetch("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token)
-    .then(res => res.json())
-    .then(user => {
-        if (utentiAutorizzati.includes(user.email)) {
-            localStorage.setItem("user", user.email);
-            window.location.href = "admin.html";
-        } else {
-            alert("Non sei autorizzato a modificare i canti!");
-        }
-    })
-    .catch(error => console.error("Errore:", error));
+    const user = decodeJwtResponse(response.credential);
+    
+    if (utentiAutorizzati.includes(user.email)) {
+        localStorage.setItem("user", user.email);
+        window.location.href = "admin.html";
+    } else {
+        alert("Non sei autorizzato a modificare i canti!");
+    }
 }
 
+// **Decodifica il token JWT di Google**
+function decodeJwtResponse(token) {
+    let base64Url = token.split('.')[1]; 
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+// **Gestione del logout**
 document.getElementById("logout").addEventListener("click", function () {
     localStorage.removeItem("user");
     window.location.href = "index.html";

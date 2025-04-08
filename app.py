8dcbe5c61b705d751422c801f94ed06bfed89614
annaficotto <ficotto.2008@gmail.com>
2025-04-08 14:37:41 +0200

    .

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def gestisci_canti():
         "data": canti_domenica,
         "domenica": datetime.now().strftime("domenica %-d %B")
     })
-
+    
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

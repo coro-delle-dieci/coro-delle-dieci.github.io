10410a564989dc2245741f1154e8a42d367d7164
annaficotto <ficotto.2008@gmail.com>
2025-04-10 21:32:31 +0200

    tolti commenti

--- support-files/add_video_numbers.py
+++ support-files/add_video_numbers.py
@@ -1,18 +1,14 @@
 def add_sections_to_html(file_path, link, n1, n2):
-    # Leggi il contenuto del file HTML
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # Trova la posizione di </section>
     section_end_index = content.find('</section>')
     if section_end_index == -1:
         print("Errore: Tag </section> non trovato nel file HTML.")
         return
 
-    # Costruisci la nuova sezione da aggiungere
     new_section = '\n'
 
-    # Aggiungi il video YouTube se il link è fornito
     if link:
         new_section += '''
         <section class="video-container">
@@ -20,7 +16,6 @@ def add_sections_to_html(file_path, link, n1, n2):
         </section>
         '''.format(link)
 
-    # Aggiungi la sezione dei numeri se almeno uno dei due è fornito
     if n1 or n2:
         new_section += '''
         <section>
@@ -33,27 +28,21 @@ def add_sections_to_html(file_path, link, n1, n2):
         new_section += '''
         </section>'''
 
-    # Inserisci la nuova sezione dopo </section>
     updated_content = content[:section_end_index + len('</section>')] + new_section + content[section_end_index + len('</section>'):]
 
-    # Scrivi il contenuto aggiornato nel file
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(updated_content)
 
     print(f"Sezioni aggiunte con successo al file: {file_path}")
 
 def main():
-    # Chiedi all'utente il percorso del file HTML
     file_path = "C:/Users/Scuola/Desktop/github/coro-delle-dieci.github.io/canti/" + input("Inserisci il percorso del file HTML: ") + ".html"
 
-    # Chiedi all'utente il link del video YouTube
     link = input("Link: ").strip().replace("https://youtu.be/", "https://www.youtube.com/embed/")
 
-    # Chiedi all'utente i numeri n1 e n2
     n1 = input("Numero nel l. minicorale: ").strip()
     n2 = input("Numero nel l. assemblea: ").strip()
 
-    # Aggiungi le sezioni al file HTML
     add_sections_to_html(file_path, link, n1, n2)
 
 if __name__ == "__main__":
--- support-files/update_list.py
+++ support-files/update_list.py
@@ -1,12 +1,9 @@
 import os
 
-# Percorso della cartella dei canti
 canti_folder = "C:/Users/Scuola/Desktop/github/coro-delle-dieci.github.io/canti"
 
-# Percorso del file HTML
 html_file = "C:/Users/Scuola/Desktop/github/coro-delle-dieci.github.io/canti.html"
 
-# Parole speciali che devono avere l'iniziale maiuscola
 special_words = {"gesù", "cristo", "dio", "signore", "signor", "santo", "spirito", "maria", "madonna", "czestochowa", "lui", "pietro"}
 
 def format_canto_name(name):
@@ -16,13 +13,10 @@ def format_canto_name(name):
     2. Capitalizza solo la prima lettera del titolo.
     3. Capitalizza le parole speciali (Gesù, Cristo, Dio) se presenti.
     """
-    # Sostituisce i trattini con spazi
     name = name.replace("-", " ").replace("l-a", "l'a").replace("l-e", "l'e").replace("l-i", "l'i").replace("l-o", "l'o").replace("l-u", "l'u").replace("m-a", "m'a")
     
-    # Capitalizza solo la prima lettera del titolo
     name = name[0].upper() + name[1:]
     
-    # Capitalizza le parole speciali
     words = name.split()
     for i, word in enumerate(words):
         if word.lower() in special_words:
@@ -31,48 +25,36 @@ def format_canto_name(name):
     
     return name
 
-# Leggi tutti i file nella cartella 'canti/'
 canti_files = [f for f in os.listdir(canti_folder) if os.path.isfile(os.path.join(canti_folder, f))]
 
-# Ordina i file alfabeticamente (opzionale)
 canti_files.sort()
 
-# Genera i link HTML per ogni canto
 canti_links = []
 for file in canti_files:
-    # Rimuovi l'estensione .html dal nome del file
     nome_canto = os.path.splitext(file)[0]
-    # Formatta il nome del canto
     nome_canto = format_canto_name(nome_canto)
-    # Crea il link HTML
     link = f'<a href="canti/{file}">{nome_canto}</a><br>'
     canti_links.append(link)
 
-# Unisci tutti i link in una stringa
 canti_links_html = '\n'.join(canti_links)
 
-# Leggi il contenuto del file HTML
 with open(html_file, 'r', encoding='utf-8') as file:
     html_content = file.read()
 
-# Trova la sezione dei canti nel file HTML
 start_marker = '<p class="canto-link">'
 end_marker = '</p>'
 start_index = html_content.find(start_marker)
 end_index = html_content.find(end_marker, start_index)
 
-# Verifica che i marcatori siano stati trovati
 if start_index == -1 or end_index == -1:
     print("Errore: Marcatori non trovati nel file HTML.")
 else:
-    # Sostituisci la sezione dei canti con la nuova lista
     new_html_content = (
         html_content[:start_index + len(start_marker)] +
         '\n' + canti_links_html + '\n' +
         html_content[end_index:]
     )
 
-    # Scrivi il nuovo contenuto nel file HTML
     with open(html_file, 'w', encoding='utf-8') as file:
         file.write(new_html_content)
 

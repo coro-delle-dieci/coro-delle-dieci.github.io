4b207580b222b49bfed8731bcf6edfb1528a5ebd
annaficotto <ficotto.2008@gmail.com>
2025-04-11 18:31:12 +0200

    aggiornata header

--- support-files/song_to_html.py
+++ support-files/song_to_html.py
@@ -58,6 +58,7 @@ def create_song_html(title, song_text):
                 <li><a href="../index.html">Home</a></li>
                 <li><a href="../canti.html">Canti</a></li>
                 <li><a href="../calendario.html">Calendario</a></li>
+                <li><a href="../errore.html">Accedi</a></li>
             </ul>
         </nav>
     </header>

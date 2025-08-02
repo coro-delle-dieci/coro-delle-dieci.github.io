ed9dbef709103028fcb115d9869558d428b7ee2c
annaficotto <ficotto.2008@gmail.com>
2025-08-02 15:02:41 +0200

    Aggiunto il foglio di stile "canti.css" nel template HTML della canzone

--- support-files/complete_song_creator.py
+++ support-files/complete_song_creator.py
@@ -86,6 +86,7 @@ def create_song_html(title, song_text, link=None, n1=None, n2=None):
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{title}</title>
     <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/canti.css">
 </head>
 <body>
     <header>

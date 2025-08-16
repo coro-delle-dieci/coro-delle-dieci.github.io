ed5537b5eab5bf58463d5b541aee91cc0e309a2e
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-08-16 14:25:59 +0200

    percorsi file aggiornati per icone e manifest nel modello HTML

--- support-files/complete_song_creator.py
+++ support-files/complete_song_creator.py
@@ -87,11 +87,11 @@ def create_song_html(title, song_text, link=None, n1=None, n2=None):
     <title>{title}</title>
     <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="../style/canti.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#061a40">
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+    <link rel="manifest" href="../images/site.webmanifest">
+    <link rel="mask-icon" href="../images/safari-pinned-tab.svg" color="#061a40">
     <meta name="msapplication-TileColor" content="#061a40">
     <meta name="theme-color" content="#061a40">
 </head>

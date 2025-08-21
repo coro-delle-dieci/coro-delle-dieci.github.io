87b7fc7ef4a5fde474b476ead094c148ce507547
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-08-21 15:47:48 +0200

    Aggiornato il piè di pagina nei file HTML della root per includere anno dinamico e disclaimer. Aggiunta una clausola di esclusione di responsabilità sulla proprietà dei testi e degli spartiti nel piè di pagina di più file HTML. Incluso un nuovo file JavaScript (anno.js) per gestire la visualizzazione dell'anno. CSS aggiornato per definire lo stile del nuovo testo di esclusione di responsabilità. Modificato uno script Python per automatizzare l'aggiornamento del piè di pagina in tutti i file HTML rilevanti.

--- support-files/substitute.py
+++ support-files/substitute.py
@@ -1,7 +1,7 @@
 import os
 
 # Percorso della cartella
-directory = r"C:/Users/ficot/Desktop/coro-delle-dieci.github.io/canti"
+directory = r"C:/Users/ficot/Desktop/coro-delle-dieci.github.io/"
 
 # Stringa da cercare e nuova stringa con cui sostituirla
 stringa_da_sostituire = '''<footer>

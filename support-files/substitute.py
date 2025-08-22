ec7d9c534686b7bf950f9f1340d8903c47731e0a
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-08-22 14:58:45 +0200

    max-width di @media modificato da 600px a 750px

--- support-files/substitute.py
+++ support-files/substitute.py
@@ -1,16 +1,11 @@
 import os
 
 # Percorso della cartella
-directory = r"C:/Users/ficot/Desktop/coro-delle-dieci.github.io/demo-palette4"
+directory = r"C:/Users/ficot/Desktop/coro-delle-dieci.github.io/style"
 
 # Stringa da cercare e nuova stringa con cui sostituirla
-stringa_da_sostituire = '''<li><a href="index/">Home</a></li>
-                <li><a href="canti/">Canti</a></li>
-                <li><a href="calendario/">Calendario</a></li>'''
-nuova_stringa = '''<li><a href="index/">Home</a></li>
-                <li><a href="canti/">Canti</a></li>
-                <li><a href="calendario/">Calendario</a></li>
-                <li><a href="../admin/aggiungi-canto/">Area riservata</a></li>'''
+stringa_da_sostituire = '''600px'''
+nuova_stringa = '''750px'''
 
 # Scorre tutti i file nella directory
 for filename in os.listdir(directory):

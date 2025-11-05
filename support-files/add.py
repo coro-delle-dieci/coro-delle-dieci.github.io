0a67b60d5778d57d5dd6b47b40024c1cf88bccf4
annaficotto <ficotto.2008@gmail.com>
2025-11-05 15:03:01 +0100

    riaggiunto counter e aggiornata privacy policy

--- support-files/add.py
+++ support-files/add.py
@@ -2,7 +2,7 @@ import os
 import re
 
 # Percorso della cartella
-directory = r"C:/Users/ficot/Desktop/coro-delle-dieci.github.io/"
+directory = r"C:/Users/ficot/Desktop/coro-delle-dieci.github.io/system/docs"
 
 # Stringa da cercare e nuova stringa con cui sostituirla
 elementi_da_aggiungere = '''\n    <script src="https://cdn.counter.dev/script.js" data-id="0b8a28a7-bd9e-4970-aa87-bd107e273a32" data-utcoffset="1"></script>'''
